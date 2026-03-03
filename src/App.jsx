abd42ac2408ccff047512c80c7f4c3ec6ba5564e
Othniel-Ken <aguidiothiniel17@gmail.com>
2026-03-03 14:03:40 +0100

    initialisation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from './components/Banner'
+import Cart from './components/Cart';
+
+const App = () => {
+  return (
+    <>
+      <Banner />
+      <Cart/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cart.jsx
@@ -0,0 +1,19 @@
+const Cart = () => {
+  const monstera = 8
+  const lierre = 10
+  const bouquet = 15
+
+  return (
+    <div>
+      <h2>Panier</h2>
+      <ul>
+        <li>Monstera : {monstera}€</li>
+        <li>Lierre : {lierre}€</li>
+        <li>Bouquet de fleurs : {bouquet}€</li>
+      </ul>
+      <p>Total : {monstera + lierre + bouquet}€</p>
+    </div>
+  )
+}
+
+export default Cart
