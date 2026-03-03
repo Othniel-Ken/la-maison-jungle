43d7aa5cadfbad6bc7c97992f7b6089239d42bb2
Othniel-Ken <aguidiothiniel17@gmail.com>
2026-03-03 14:57:00 +0100

    Style composant Cart

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,14 @@
+import '../styles/Banner.css'
+import logo from '../assets/logo.png'
+
 const Banner = () => {
-  return <h1>🌿 La maison jungle</h1>
+  const title = 'La maison jungle'
+  return (
+    <div className="banner">
+      <img src={logo} alt="La maison jungle logo" className="banner-logo" />
+      <h1 className="banner-title">{title}</h1>
+    </div>
+  )
 }
 
 export default Banner
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,12 @@
+import '../styles/Cart.css'
+
 const Cart = () => {
   const monstera = 8
   const lierre = 10
   const bouquet = 15
 
   return (
-    <div>
+    <div className='cart'>
       <h2>Panier</h2>
       <ul>
         <li>Monstera : {monstera}€</li>
